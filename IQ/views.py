ae619d30a47d030d40394cf7d7add91ba3e7ee2b
Diyorbek <majidovdiyorbek741@gmail.com>
2023-10-31 10:21:51 +0500

    user reistrations page

--- IQ/views.py
+++ IQ/views.py
@@ -6,28 +6,43 @@ from .serializers import UserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.hashers import make_password
 import requests
+import random
+from .models import User
 url = "http://notify.eskiz.uz/api/message/sms/send"
 
 class Registration(APIView):
     def post(self, request):
         data = request.data
         number = data["mobile_number"]
-        data["password"] = make_password(request.data['password'])
-        serializer = UserSerializer(data)
+        state = random.getstate()
+        
         payload={'mobile_phone': number,
-        'message': 'Eskiz Test',
+        'message': f'Eskiz Test {(random.setstate(state),4)*100000}',
         'from': '4546',
         'callback_url': 'http://0000.uz/test.php'}
         files=[]
         token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjI0NTksInJvbGUiOm51bGwsImRhdGEiOnsiaWQiOjI0NTksIm5hbWUiOiJPXHUyMDE4emJla2lzdG9uLUZpbmxhbmRpeWEgcGVkYWdvZ2lrYSBpbnN0aXR1dGkiLCJlbWFpbCI6ImRvc3VtYmV0b3YxOTk4MzAxMEBnbWFpbC5jb20iLCJyb2xlIjpudWxsLCJhcGlfdG9rZW4iOiJleUowZVhBaU9pSktWMVFpTENKaGJHY2lPaUpJVXpJMU5pSjkuZXlKemRXSWlPakkwTlRrc0luSnZiR1VpT2lJaUxDSmtZWFJoSWpwN0ltbGtJam95TkRVNUxDSnVZVzFsSWpvaVJHOXpkVzFpWlhSdmRpQlBaeWRoWW1WcklFUmhkbXhoZENCdkoyY25iR2tpTENKbGJXRnBiQ0k2SW1SdmMzVnRZbVYwYjNZeE9UazRNekF4TUVCbmJXRnBiQzVqYjIwaUxDSnliMnhsSWpvaUlpd2lZWEJwWDNSdmEyVnVJam9pWlhsS01HVllRV2xQYVVwTFZqRlJhVXhEU20iLCJzdGF0dXMiOiJhY3RpdmUiLCJzbXNfYXBpX2xvZ2luIjoiZXNraXoyIiwic21zX2FwaV9wYXNzd29yZCI6ImUkJGsheiIsInV6X3ByaWNlIjo1MCwidWNlbGxfcHJpY2UiOjExNSwidGVzdF91Y2VsbF9wcmljZSI6MCwiYmFsYW5jZSI6OTgwNjUwLCJpc192aXAiOjAsImhvc3QiOiJzZXJ2ZXIxIiwiY3JlYXRlZF9hdCI6IjIwMjMtMDItMDdUMTQ6Mzg6MjMuMDAwMDAwWiIsInVwZGF0ZWRfYXQiOiIyMDIzLTEwLTA5VDA1OjQzOjA3LjAwMDAwMFoiLCJ3aGl0ZWxpc3QiOm51bGwsImhhc19wZXJmZWN0dW0iOjAsImJlZWxpbmVfcHJpY2UiOjUwfSwiaWF0IjoxNjk2ODMwOTQ1LCJleHAiOjE2OTk0MjI5NDV9.JsPtMAFIPL9oHKkGYVbHlhTLJAz99RbsDmyY8KYxF94"
         headers = {'Authorization': "Bearer {}".format(token)}
-        if serializer.is_valid():
-            serializer.save()
+
+        data['password'] = make_password(number)
+        serializer = UserSerializer(data=data)
+        user=User.objects.filter(mobile_number=number).last()
+        if user.DoesNotExist:
+            token = RefreshToken.for_user(user)
+
+            context = {
+                "refresh":str(token),
+                "access": str(token.access_token),
+                "sms": requests.request("POST", url, headers=headers, data=payload, files=files).text
+            }
+            return Response(context)
+        elif serializer.is_valid():
+            serializer.save
             token = RefreshToken.for_user(serializer.instance)
             context = {
                 "refresh":str(token),
                 "access": str(token.access_token),
-                "sms": requests.request("POST", url, headers=headers, data=payload, files=files)
+                "sms": requests.request("POST", url, headers=headers, data=payload, files=files).text
             }
             return Response(context)
         return Response(serializer.errors)
--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'IQ.apps.IqConfig'
 ]
 
@@ -55,8 +56,8 @@ from datetime import timedelta
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ROTATE_REFRESH_TOKENS": False,
-    "BLACKLIST_AFTER_ROTATION": False,
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True ,
     "UPDATE_LAST_LOGIN": False,
 
     "ALGORITHM": "HS256",
