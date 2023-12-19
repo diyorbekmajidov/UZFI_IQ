304f9036d18924dd67661f2d2f23a72b710088af
Diyorbek <majidovdiyorbek741@gmail.com>
2023-12-19 09:22:55 +0500

    1

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
+from IQ.views import Index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+]+i18n_patterns (
+    path('i18n/', include('django.conf.urls.i18n')),
     path('iq/', include("IQ.urls")),
-]
+    # path('', Index.as_view()),
+)
