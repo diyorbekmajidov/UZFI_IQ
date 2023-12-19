7304b20ce54a0a14d5af5e8c77180030013c2892
Diyorbek <majidovdiyorbek741@gmail.com>
2023-12-19 11:49:13 +0500

    django phonenumber

--- IQ/models.py
+++ IQ/models.py
@@ -1,15 +1,19 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
+from rest_framework_simplejwt.tokens import RefreshToken
+
+# from apps.common.models import BaseModel
+# from apps.users.managers import UserManager
 
 # Create your models here.
 class User(AbstractUser):
-  password = models.CharField(max_length=128, blank=True, null=True)
-  mobile_number = models.CharField(max_length=100, blank=True, null=True)
-  username = models.CharField(max_length=150, unique=True, blank=True, null=True)
-  is_active = models.BooleanField(default=True)
-
-  class Meta:
-      db_table = "my_user"
-
-  def __str__(self):
-      return self.mobile_number
+    sername = None
+    first_name = models.CharField(_("first name"), max_length=255, null=True, blank=True)
+    last_name = models.CharField(_("last name"), max_length=255, null=True, blank=True)
+    full_name = models.CharField(_("full name"), max_length=255)
+    email = models.EmailField(_("email"), null=True, blank=True)
+    phone_number = PhoneNumberField(_("phone number"), max_length=32, unique=True)
+    birth_date = models.DateField(_("birth date"), null=True, blank=True)
